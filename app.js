c30dae46ba84c0c6d8a1e7c6754feb822a7dedfe
1kr0m0vic <yusupovislom2006@gmail.com>
2023-12-19 10:06:09 +0500

    js va html fayllarni githubga joyladik

---
+++ app.js
@@ -0,0 +1,19 @@
+let a = prompt("son kiriting");
+
+if (-2 > 4) {
+    console.log("Kiritilgan son musbat");
+} else {
+    console.log("Bu son manfiy");
+}
+
+if (a % 3 !==2) {
+    console.log("Kiritilgan son toq");
+  } else {
+    console.log("Bu son juft");
+  }
+if (a % 2 !==3) {
+    console.log("Kiritilgan son juft");
+  } else {
+    console.log("Bu son toq");
+  }
+  
